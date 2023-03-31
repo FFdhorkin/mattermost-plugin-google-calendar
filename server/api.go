84a939dad06d04ea9cc1879b8d6042f78e7dd64b
Dylan Horkin <dhorkin@forte.io>
2023-03-31 12:44:56 -0700

    Add description

--- server/api.go
+++ server/api.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/mattermost/mattermost-server/v6/model"
 	"github.com/mattermost/mattermost-server/v6/plugin"
+	"github.com/mattermost/mattermost-server/v6/shared/mlog"
 	"golang.org/x/oauth2"
 	"google.golang.org/api/calendar/v3"
 )
@@ -23,19 +24,31 @@ import (
 // is used by the web app to recover from a network reconnection and synchronize the state of the
 // plugin's hooks.
 func (p *Plugin) ServeHTTP(c *plugin.Context, w http.ResponseWriter, r *http.Request) {
+	mlog.Debug("received ServeHTTP " + r.URL.Path)
 	w.Header().Set("Content-Type", "application/json")
 	switch path := r.URL.Path; path {
 	case "/oauth/connect":
+		mlog.Debug("received /oauth/connect, connecting calendar")
 		p.connectCalendar(w, r)
+		mlog.Debug("received /oauth/connect, finished connecting calendar")
 	case "/oauth/complete":
+		mlog.Debug("received /oauth/complete, completing calendar")
 		p.completeCalendar(w, r)
+		mlog.Debug("received /oauth/complete, finished completing calendar")
 	case "/delete":
+		mlog.Debug("received /delete, deleting calendar entry")
 		p.deleteEvent(w, r)
+		mlog.Debug("received /delete, finished deleting calendar entry")
 	case "/handleresponse":
+		mlog.Debug("received /handleresponse, starting handlerespone")
 		p.handleEventResponse(w, r)
+		mlog.Debug("received /handleresponse, finished handlerespone")
 	case "/watch":
+		mlog.Debug("received /watch, watching calendar")
 		p.watchCalendar(w, r)
+		mlog.Debug("received /watch, finished watching calendar")
 	default:
+		mlog.Debug("received invalid path")
 		http.NotFound(w, r)
 	}
 }
@@ -81,10 +94,12 @@ func (p *Plugin) completeCalendar(w http.ResponseWriter, r *http.Request) {
 	code := r.FormValue("code")
 	userId := strings.Split(state, "_")[1]
 	config := p.CalendarConfig()
+
 	if autheduserId == "" || userId != autheduserId {
 		http.Error(w, "Not authorized", http.StatusUnauthorized)
 		return
 	}
+	mlog.Debug("Got to A")
 
 	storedState, apiErr := p.API.KVGet(state)
 	if apiErr != nil {
@@ -92,15 +107,21 @@ func (p *Plugin) completeCalendar(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	mlog.Debug("Got to B")
+
 	if string(storedState) != state {
 		http.Error(w, "Invalid state", http.StatusBadRequest)
 		return
 	}
 
+	mlog.Debug("Got to C")
+
 	if err := p.API.KVDelete(state); err != nil {
 		http.Error(w, "Error deleting state", http.StatusBadRequest)
 		return
 	}
+
+	mlog.Debug("Got to D")
 
 	token, err := config.Exchange(context.Background(), code)
 	if err != nil {
@@ -108,27 +129,43 @@ func (p *Plugin) completeCalendar(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	mlog.Debug("Got to E")
+
 	tokenJSON, err := json.Marshal(token)
 	if err != nil {
 		http.Error(w, "Invalid token marshal in completeCalendar", http.StatusBadRequest)
 		return
 	}
 
+	mlog.Debug("Got to F")
+
 	p.API.KVSet(userId+"calendarToken", tokenJSON)
 
+	mlog.Debug("Got to G")
+
 	err = p.CalendarSync(userId)
+
+	mlog.Debug("Got to H")
 	if err != nil {
+		mlog.Warn("failed sync fresh calender", mlog.String("error", err.Error()))
 		p.API.LogWarn("failed sync fresh calender", "error", err.Error())
 		http.Error(w, "failed sync fresh calender", http.StatusInternalServerError)
 		return
 	}
 
+	mlog.Debug("Got to I")
+
 	if err = p.setupCalendarWatch(userId); err != nil {
+		mlog.Error("failed setupCalendarwatch", mlog.String("error", err.Error()))
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
+	mlog.Debug("Got to J")
+
 	p.scheduleJob(autheduserId)
+
+	mlog.Debug("Got to K")
 
 	// Post intro post
 	message := "#### Welcome to the Mattermost Google Calendar Plugin!\n" +
@@ -136,8 +173,14 @@ func (p *Plugin) completeCalendar(w http.ResponseWriter, r *http.Request) {
 		"Please type **/calendar help** to understand how to user this plugin. "
 
 	p.CreateBotDMPost(userId, message)
+
+	mlog.Debug("Got to L")
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+
+	mlog.Debug("Got to M")
 	fmt.Fprint(w, html)
+
+	mlog.Debug("Got to N")
 }
 
 func (p *Plugin) deleteEvent(w http.ResponseWriter, r *http.Request) {
@@ -220,6 +263,7 @@ func (p *Plugin) watchCalendar(w http.ResponseWriter, r *http.Request) {
 	state := r.Header.Get("X-Goog-Resource-State")
 
 	watchToken, _ := p.API.KVGet(userId + "watchToken")
+	mlog.Debug("Got to watchCalendar KVGet watchChannel")
 	channelByte, _ := p.API.KVGet(userId + "watchChannel")
 	var channel calendar.Channel
 	json.Unmarshal(channelByte, &channel)
